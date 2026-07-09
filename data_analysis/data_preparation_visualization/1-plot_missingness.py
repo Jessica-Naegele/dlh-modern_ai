dd0246ff3cf6cb337d1a7a55a0be42abd67e01d3
Jessica-Naegele <jessica.a.naegele@gmail.com>
2026-07-09 16:57:26 +0200

    9-1-1

--- data_analysis/data_preparation_visualization/1-plot_missingness.py
+++ data_analysis/data_preparation_visualization/1-plot_missingness.py
@@ -13,37 +13,27 @@ def plot_missingness(df):
     The x-axis represents row indices (DataFrame records)
     The y-axis represents column names.
     Y-tick labels are explicitly mapped to the DataFrame column names.
-    Each missing value is displayed as a vertical bar (|), using the default plotting color.
+    Each missing value is displayed as a vertical bar
+    (|), using the default plotting color.
     Displays the plot using Matplotlib
     x Returns: None
     """
     plt.figure(figsize=(12, 8))
 
-
     # your code here
     df_nul = df.isnull()
-    print(df_nul) #helper
-    nul = np.where(df_nul == True)
-    # print(nul) # 'helper'
-    
+    # print(df_nul)  # helper
+    nul = np.where(df_nul)
+    # print(nul)  # 'helper'
+
     x = nul[0]
     y = nul[1]
-    
-    plt.scatter(x, y)
+
+    plt.scatter(x, y, marker='|')
     # print(df.columns.values)
-    
+
     plt.title("Missingness Plot")
     plt.yticks(np.arange(0, len(df.columns.values)), df.columns.values)
-    """
-    # scat = df.plot.scatter(x="XACHSIS", y='YACHSIS)
-    
-   
-    
-    
-    plt.title('TITLE') #needs to be updated
-    """
-
-
 
     plt.tight_layout()
     plt.show()
