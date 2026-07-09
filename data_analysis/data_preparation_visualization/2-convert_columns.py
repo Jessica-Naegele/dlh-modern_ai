8f7d20026b9114bb8b7a1430667abdf24e009dd3
Jessica-Naegele <jessica.a.naegele@gmail.com>
2026-07-09 17:17:17 +0200

    9-0-2

--- data_analysis/data_preparation_visualization/2-convert_columns.py
+++ data_analysis/data_preparation_visualization/2-convert_columns.py
@@ -9,7 +9,8 @@ def convert_columns(df):
     df: pandas DataFrame containing the columns TotalCharges and SeniorCitizen
     Converts the TotalCharges column to numeric.
     Non-numeric entries should be converted to NaN
-    Maps the numeric values in the SeniorCitizen column (0 and 1) to categorical strings "No" and "Yes" respectively
+    Maps the numeric values in the SeniorCitizen column
+    (0 and 1) to categorical strings "No" and "Yes" respectively
     Returns: The modified DataFrame
     """
     df['TotalCharges'] = pd.to_numeric(df['TotalCharges'], errors='coerce')
