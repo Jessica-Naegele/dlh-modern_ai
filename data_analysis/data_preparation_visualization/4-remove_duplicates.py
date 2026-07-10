5cb6b2515ce0f3c4140e91fd69ef1e596ed02e30
Jessica-Naegele <jessica.a.naegele@gmail.com>
2026-07-10 18:27:21 +0200

    9-1-4

--- data_analysis/data_preparation_visualization/4-remove_duplicates.py
+++ data_analysis/data_preparation_visualization/4-remove_duplicates.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
 """creating a function to remove duplicate rows"""
 
+
 def remove_duplicates(df):
     """Function removing duplicate rows"""
     df_cleaned = df.drop_duplicates()
