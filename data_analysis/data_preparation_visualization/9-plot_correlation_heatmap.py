d5975d6c8fc41ac1f96b3c2d8d4fa405cf2f3c52
Jessica-Naegele <jessica.a.naegele@gmail.com>
2026-07-11 15:44:06 +0200

    9-1-9

--- data_analysis/data_preparation_visualization/9-plot_correlation_heatmap.py
+++ data_analysis/data_preparation_visualization/9-plot_correlation_heatmap.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
 """
-Creating a function visualizing correlations between continuous numeric features
+Creating a function visualizing correlations between continuous
+numeric features
 """
 import seaborn as sns
 import matplotlib.pyplot as plt
@@ -12,19 +13,20 @@ def plot_correlation_heatmap(df):
     df: pandas DataFrame
     Computes pairwise correlations
     Generates an annotated heatmap with coolwarm colormap
-    Set vmin = -1 and vmax = 1 so that the heatmap color mapping reflects the full correlation range
+    Set vmin = -1 and vmax = 1 so that the heatmap color
+    mapping reflects the full correlation range
     Displays the plot
     Returns: None
     """
     plt.figure(figsize=(6, 5))
 
-    #your code here
     # only continuous numeric features
     df_plot = df.select_dtypes(include=['number'])
     # needs pairwise correlation matrix
     corr_matrix = df_plot.corr()
 
-
-    #set up the heatmap
+    # set up the heatmap
     sns.heatmap(corr_matrix, annot=True, cmap="coolwarm", vmin=-1, vmax=1)
+    plt.title("Correlation Matrix")
+    # let's plot
     plt.show()
