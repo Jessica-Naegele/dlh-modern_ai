65d8de30ff5b1a4b652acceedd7b58ad8376f661
Jessica-Naegele <jessica.a.naegele@gmail.com>
2026-07-12 12:52:08 +0200

    9-1-10 it 5 using yes and no

--- data_analysis/data_preparation_visualization/10-plot_categorical_vs_churn.py
+++ data_analysis/data_preparation_visualization/10-plot_categorical_vs_churn.py
@@ -5,11 +5,6 @@ import matplotlib.pyplot as plt
 
 def plot_categorical_vs_churn(df, col):
     """visualizing churn rates per category"""
-
-    # change the Churn values from Yes = 1 & No = 0
-    df_copy = df.copy()
-    # df_copy['Churn'] = df_copy['Churn'].replace({'Yes': 1, 'No': 0})
-
     # Group by category, get % of Yes and No within each group
     churn_rate = df.groupby(col)['Churn'].value_counts(normalize=True)
     # Filter just the data whit yes churn
@@ -18,11 +13,9 @@ def plot_categorical_vs_churn(df, col):
     values = churn_rate_yes.values
     # bar chart
     plt.figure(figsize=(12, 8))
-    ax = churn_rate_yes.plot.bar(x=labels, y=values, ylabel='Churn Rate')
-    ax.set_title(f'Churn Rate by {col}')
-    ax.tick_params(axis='x', rotation=45)
-    ax.set_xlabel("")
-
+    plt.bar(labels, values)
+    plt.ylabel('Churn Rate')
+    plt.title(f'Churn Rate by {col}')
+    plt.xticks(rotation=45)
     plt.show()
-
     return None
