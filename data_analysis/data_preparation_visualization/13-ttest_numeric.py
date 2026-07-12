248692ca4f87fa2dc7bbd705c41a9a5d240a8a04
Jessica-Naegele <jessica.a.naegele@gmail.com>
2026-07-12 18:29:28 +0200

    9-1-13

--- data_analysis/data_preparation_visualization/13-ttest_numeric.py
+++ data_analysis/data_preparation_visualization/13-ttest_numeric.py
@@ -6,21 +6,23 @@ from scipy import stats
 
 def ttest_numeric(df):
     """Function performing Welch's ttest
-    Computes t-test p-value comparing Churn=Yes vs Churn=No for each numeric feature
+    Computes t-test p-value comparing Churn=Yes vs Churn=No for
+    each numeric feature
     The Hypothesis being tested is:
-    H₀ (null): The means of the variable are equal in Churn=Yes and Churn=No groups
+    H₀ (null): The means of the variable are equal in Churn=Yes
+    and Churn=No groups
     H₁ (alternative): The means differ significantly
-    Returns a dictionary: {feature_name: p_value}  
+    Returns a dictionary: {feature_name: p_value}
     """
     # only numeric features
     df_select = df.select_dtypes(include=['number']).copy()
 
-    #hypothesisis
+    # hypothesisis
 
     """To perform Welch's t-test in Python using the SciPy library,
      use the scipy.stats.ttest_ind function with the argument
-     equal_var=False"""    
-    
+     equal_var=False"""
+
     ttest = {}
 
     for column in df_select:
@@ -29,5 +31,5 @@ def ttest_numeric(df):
         result = stats.ttest_ind(group1, group2, equal_var=False)
 
         ttest[column] = result.pvalue
-    
+
     return ttest
