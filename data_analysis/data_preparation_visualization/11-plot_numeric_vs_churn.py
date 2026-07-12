f345e6da2d2d26a1897cd008c4aaa838c6619bf9
Jessica-Naegele <jessica.a.naegele@gmail.com>
2026-07-12 15:14:06 +0200

    .\11-plot_numeric_vs_churn.py

--- data_analysis/data_preparation_visualization/11-plot_numeric_vs_churn.py
+++ data_analysis/data_preparation_visualization/11-plot_numeric_vs_churn.py
@@ -1,7 +1,6 @@
 #!/usr/bin/env python3
 """function comparing continiuous features against Churn"""
 import matplotlib.pyplot as plt
-import seaborn as sns
 
 
 def plot_numeric_vs_churn(df, col):
@@ -9,17 +8,29 @@ def plot_numeric_vs_churn(df, col):
     df : DataFrame with Churn
     col: numeric column name
     """
+    """version 1    # 1. Group, count into 30 bins, and flatten
+    churn_rate = df.groupby('Churn')[col].value_counts(sort=False, bins=30)
+    churn_rate_un = churn_rate.reset_index()
+
+    # Pivot it with No and Yes as columns
+    churn_piv = churn_rate_un.pivot(index=col, columns='Churn', values='count')
+
+    """
+
+    # version 2
+    col_no = df[df['Churn'] == 'No'][col]
+    col_yes = df[df['Churn'] == 'Yes'][col]
 
     # using a figure size (12, 8)
     plt.figure(figsize=(12, 8))
 
-    sns.histplot(
-        data=df, x=col, hue='Churn', bins=30, multiple='dodge', shrink=0.8
-    )
+    # plotting with the pivot
+    plt.hist([col_no, col_yes], bins=30, label=['No', 'Yes'])
 
-    #add titles and labels
+    # add titles and labels
     plt.title(f'{col} Distribution by Churn')
     plt.xlabel(col)
-    plt.ylabel("Count")
+    # plt.ylabel("Count")
+    plt.legend(title='Churn')
 
     plt.show()
