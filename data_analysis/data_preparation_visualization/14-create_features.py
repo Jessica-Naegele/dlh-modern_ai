88299190321311b24593ccc44c8213c0c43da51a
Jessica-Naegele <jessica.a.naegele@gmail.com>
2026-07-12 21:05:59 +0200

    9-1-14

--- data_analysis/data_preparation_visualization/14-create_features.py
+++ data_analysis/data_preparation_visualization/14-create_features.py
@@ -8,8 +8,11 @@ def create_features(df):
     """function engineering new features from dataset"""
 
     # 1 Numservices
-    # Number of services customer is subscribed to 
-    target_cols = [col for col in df.columns if 'No internet service' in df[col].values]
+    # Number of services customer is subscribed to
+    target_cols = [
+        col for col in df.columns if
+        'No internet service' in df[col].values
+        ]
     target_cols.append('YNInternet')
 
     # creating a help row for InternetServices
@@ -20,17 +23,20 @@ def create_features(df):
             'Fiber optic': 'Yes'
             }
         )
-    
+
     # creating NumServices
     df['NumServices'] = df[target_cols].eq('Yes').sum(axis=1)
 
-    #2 TenureGroup
+    # 2 TenureGroup
     # Define the numeric boundary edges of your buckets
     bins = [0, 12, 24, 48, 60, float('inf')]
 
     # Define the category names matching those buckets
     labels = ['0-12', '13-24', '25-48', '49-60', '60+']
-    df['TenureGroup'] = pd.cut(df['tenure'], bins=bins, labels=labels, include_lowest=False)
+    df['TenureGroup'] = pd.cut(
+        df['tenure'], bins=bins, labels=labels,
+        include_lowest=False
+        )
 
     # Drop original columns
     df.drop(columns=target_cols, inplace=True)
