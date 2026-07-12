42875ab1708a23fc7fb6f452187d23230a8d8525
Jessica-Naegele <jessica.a.naegele@gmail.com>
2026-07-12 12:46:24 +0200

    9-1-10 it 5 using yes and no

--- data_analysis/data_preparation_visualization/10-plot_categorical_vs_churn.py
+++ data_analysis/data_preparation_visualization/10-plot_categorical_vs_churn.py
@@ -8,17 +8,17 @@ def plot_categorical_vs_churn(df, col):
 
     # change the Churn values from Yes = 1 & No = 0
     df_copy = df.copy()
-    df_copy['Churn'] = df_copy['Churn'].replace({'Yes': 1, 'No': 0})
+    # df_copy['Churn'] = df_copy['Churn'].replace({'Yes': 1, 'No': 0})
 
-    # how many % said Churn = Yes based on different categories
-    values = df_copy.groupby(col)['Churn'].agg('mean').to_list()
-    labels = df_copy.groupby(col)['Churn'].agg('mean').index.to_list()
-
-    df_group = df_copy.groupby(col)['Churn'].mean()
-
+    # Group by category, get % of Yes and No within each group
+    churn_rate = df.groupby(col)['Churn'].value_counts(normalize=True)
+    # Filter just the data whit yes churn
+    churn_rate_yes = churn_rate[:, 'Yes']
+    labels = churn_rate_yes.index
+    values = churn_rate_yes.values
     # bar chart
     plt.figure(figsize=(12, 8))
-    ax = df_group.plot.bar(x=labels, y=values, ylabel='Churn Rate')
+    ax = churn_rate_yes.plot.bar(x=labels, y=values, ylabel='Churn Rate')
     ax.set_title(f'Churn Rate by {col}')
     ax.tick_params(axis='x', rotation=45)
     ax.set_xlabel("")
