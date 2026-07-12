e85fd63d2c856c791a8c90dc73aebccb83ce9980
Jessica-Naegele <jessica.a.naegele@gmail.com>
2026-07-12 22:26:48 +0200

    9-1-17

--- data_analysis/data_preparation_visualization/17-split_data.py
+++ data_analysis/data_preparation_visualization/17-split_data.py
@@ -19,7 +19,7 @@ def split_data(df, target='Churn', test_size=0.2, random_state=42):
     # isolate target vector (y)
     y = df[target]
 
-    # perform stratified split 
+    # perform stratified split
     X_train, X_test, y_train, y_test = model_selection.train_test_split(
         X,
         y,
