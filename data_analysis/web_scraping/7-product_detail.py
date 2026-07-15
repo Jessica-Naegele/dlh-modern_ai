f63a08c378e0f2234310b40e353a580bbcf8e160
Jessica-Naegele <jessica.a.naegele@gmail.com>
2026-07-15 20:13:42 +0200

    10-0-7

--- data_analysis/web_scraping/7-product_detail.py
+++ data_analysis/web_scraping/7-product_detail.py
@@ -9,9 +9,9 @@ def scrape_product_detail(url, delay=2.0):
     """
     "title": the product title (the second <h4> inside .caption)
     "price": the price (text of the first <h4 class="price">)
-    "description": the full description (text of 
+    "description": the full description (text of
     <p class="description">)
-    "rating": the number of stars (count of 
+    "rating": the number of stars (count of
     <p class="ws-icon ws-icon-star"> in .ratings)
     """
     # set up browser and browser options
@@ -33,15 +33,21 @@ def scrape_product_detail(url, delay=2.0):
     try:
         # product_cards = driver.find_elements("class name", "caption")
         # "title": the product title (the second <h4> inside .caption)
-        title = driver.find_element("css selector", ".caption h4:nth-of-type(2)").text.strip()
+        title = driver.find_element(
+            "css selector", ".caption h4:nth-of-type(2)"
+            ).text.strip()
         # "price": the price (text of the first <h4 class="price">)
         price = driver.find_element("class name", "price").text
-        # "description": the full description (text of 
+        # "description": the full description (text of
         # <p class="description">)
-        description = driver.find_element("class name", "description").text.strip()
-        # "rating": the number of stars (count of 
+        description = driver.find_element(
+            "class name", "description"
+            ).text.strip()
+        # "rating": the number of stars (count of
         # <p class="ws-icon ws-icon-star"> in .ratings)
-        rating_element = driver.find_elements("css selector", ".ratings .ws-icon-star")
+        rating_element = driver.find_elements(
+            "css selector", ".ratings .ws-icon-star"
+            )
         rating = len(rating_element)
 
         # print(f"rating_element: {rating_element}")
@@ -57,6 +63,5 @@ def scrape_product_detail(url, delay=2.0):
         # print(product)
     finally:
         driver.quit()
-    
+
     return product
-    
