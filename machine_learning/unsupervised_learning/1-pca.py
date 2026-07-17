6cc3a12c830fc0c83e77c37fbad5635667f0eaaa
Jessica-Naegele <jessica.a.naegele@gmail.com>
2026-07-17 15:29:19 +0200

    10-2-1

--- machine_learning/unsupervised_learning/1-pca.py
+++ machine_learning/unsupervised_learning/1-pca.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 """
 --- TASK 1 ---
-function performing PCA 
+function performing PCA
 """
 
 from sklearn import decomposition
@@ -18,13 +18,14 @@ def Apply_PCA(X, n_components, random_state):
         - float [0-1] Minimum fraction
         - None: Keep all components
     - random_state (int) - random seed for reproducibility
-
     Returns:
     - numpy.ndarray (data transformed)
     - fitted pca instance
     """
 
-    pca = decomposition.PCA(n_components=n_components, random_state=random_state)
+    pca = decomposition.PCA(
+        n_components=n_components, random_state=random_state
+        )
     pca_fit = pca.fit(X)
     df_pca = pca_fit.transform(X)
 
