d66c2d155a77a4eb9090cf32b06c0e1bff080426
Jessica-Naegele <jessica.a.naegele@gmail.com>
2026-07-17 15:06:39 +0200

    10-2-1

---
+++ machine_learning/unsupervised_learning/0-standardize.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python3
+"""
+--- TASK 0 ---
+function standardizing tabular data using Scikit learn
+all feature contribute proportionaly
+"""
+
+from sklearn import preprocessing
+
+
+def Standardize(X):
+    """
+    args:
+    - X (numpy.ndarray) - shape: (n_samples, n_features)
+     --> test for negative cases
+
+     return:
+     - numpy.ndarray = standardized version of input data 
+     - same shape as X
+    
+    """
+    scaler = preprocessing.StandardScaler()
+    standardized_data = scaler.fit_transform(X)
+
+    return standardized_data
+
