522dcee4c60a0196a221dbe2faddd3546402ed39
Jessica-Naegele <jessica.a.naegele@gmail.com>
2026-07-21 15:04:46 +0200

    11-0-1

---
+++ machine_learning/tree_models/1-train.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+"""
+--- TASK 1 ---
+train a tree-based classifier
+"""
+
+
+def train_tree(clf, X, y):
+    """
+    function training a tree-based classifier
+    
+    args:
+    - clf: scikit-learn classifier instance
+    - X: input features
+    y: Target labels
+    """
+
+    return clf.fit(X,y)
