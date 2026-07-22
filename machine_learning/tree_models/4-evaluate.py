10904c76f21845f781a7325bfc271bd019ef9564
Jessica-Naegele <jessica.a.naegele@gmail.com>
2026-07-22 12:17:26 +0200

    11-0-4

---
+++ machine_learning/tree_models/4-evaluate.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python3
+"""
+--- TASK 4 ---
+creating a classification report
+"""
+
+from sklearn import metrics
+
+
+def evaluate(true_labels, predicted_labels, class_names):
+    """
+    function generating a detailed classification
+
+    args:
+    - true_labels: ground truth labels
+    - predicted_labels
+    - class_names: list of class names corresponding to label indices
+
+    return:
+    - sring
+    - precision
+    - recall
+    - f1-score
+    """
+
+    report = metrics.classification_report(y_true= true_labels, y_pred= predicted_labels,target_names=class_names )
+
+    return report
