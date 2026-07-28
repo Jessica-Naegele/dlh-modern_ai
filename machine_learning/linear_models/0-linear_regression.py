a3c227d5d8d2b0ceb8e1da62e1d8a264cf7ef96c
Jessica-Naegele <jessica.a.naegele@gmail.com>
2026-07-28 19:21:44 +0200

    12-0-0

---
+++ machine_learning/linear_models/0-linear_regression.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+"""
+--- TASK 0 ---
+returning untrained instance
+"""
+
+from sklearn import linear_model
+
+
+def Linear_Regression():
+    """
+    creating a linear regression model
+    - ordinary scares to fit a linear model to the data
+    - args: None
+    """
+
+    return linear_model.LinearRegression()
