8e1043e06c1c031a4b7063074adcf94715894184
Jessica-Naegele <jessica.a.naegele@gmail.com>
2026-07-28 20:31:10 +0200

    12-0-2

---
+++ machine_learning/linear_models/2-ridge_regression.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python3
+"""
+--- TASK 2 ---
+function creating Ridge Regression model
+"""
+
+from sklearn import linear_model
+
+
+def ridge_regression(random_state):
+    """
+    Ridge Regresion - extends ordinary linear regression by adding
+    L2 regularization, which helps stabilize the model
+    by shrinking large coefficients
+
+    args:
+    - random stage: int - random seed for reproducibility
+
+    return: 
+    - model: untrained Ridge
+    """
+
+    model = linear_model.Ridge(random_state=random_state)
+
+    return model
