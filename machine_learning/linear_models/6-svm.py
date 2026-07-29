9aae909582164963bfc345312ca71d082e065371
Jessica-Naegele <jessica.a.naegele@gmail.com>
2026-07-29 10:18:05 +0200

    12-0-6

---
+++ machine_learning/linear_models/6-svm.py
@@ -0,0 +1,33 @@
+#!/usr/bin/env python3
+"""
+--- TASK 6 ---
+Support Vector Machine classifier with specified kernel
+"""
+
+from sklearn import svm
+
+
+def get_SVM_model(name, random_state):
+    """
+    Support Vector Machine (SVM) classifier
+
+    - name: string indicating type of model
+        - linear
+        - poly
+        - rbf
+    - random_state int
+    
+    return:
+    - untrained SVC
+    """
+
+    if name == 'linear':
+        svc = svm.SVC(kernel='linear', random_state=random_state)
+    elif name == 'poly':
+        svc = svm.SVC(kernel='poly', random_state=random_state)
+    elif name == 'rbf':
+        svc = svm.SVC(kernel='rbf', random_state=random_state)
+    else:
+        raise ValueError('name not allowed')
+
+    return svc
