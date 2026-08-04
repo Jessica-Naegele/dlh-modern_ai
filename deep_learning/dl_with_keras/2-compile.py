283830f5fc394e5079c0d739cb80f4d8de29a97a
Jessica-Naegele <jessica.a.naegele@gmail.com>
2026-08-04 17:41:28 +0200

    13-0-2

---
+++ deep_learning/dl_with_keras/2-compile.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python3
+"""
+--- TASK 2 ---
+creating a model fr training
+"""
+
+from tensorflow import keras
+
+
+def compile_model(model, learning_rate=0.01):
+    """
+    model for training having:
+    - Optimizer: stochastic gradient descent 
+    - Loss function: binary cross-entropy
+    - Classification performance: accuracy
+
+    args:
+    - model
+    - learning_rate: for gradient descent
+
+    return: None
+    """
+    model.compile(
+        optimizer=keras.optimizers.SGD(learning_rate=learning_rate) ,
+        loss=keras.losses.BinaryCrossentropy(),
+        metrics = [keras.metrics.Accuracy()]
+    )
+
+    return None
