6f502b7ae9ec8d29cd79333aad7c511d89fdd53c
Jessica-Naegele <jessica.a.naegele@gmail.com>
2026-08-05 15:02:05 +0200

    13-0-5

---
+++ deep_learning/dl_with_keras/5-save_load_model.py
@@ -0,0 +1,39 @@
+#!/usr/bin/env python3
+"""
+-- Task 5 --
+2 functions:
+- save model
+- load model
+"""
+
+
+def save_model(model, filepath):
+    """
+    function to save a model (architecture, weights and optimizer)
+
+    args:
+    - model: trained model
+    - filepath: string including file path & file name
+
+    return:
+    - NONE   
+    """
+
+    model.save(filepath)
+
+    return None
+
+
+def load_model(filepath):
+    """
+    function loading a model
+
+    args:
+    - filepath
+
+    return: model
+    """
+
+    model = keras.models.load_model(filepath)
+
+    return model
