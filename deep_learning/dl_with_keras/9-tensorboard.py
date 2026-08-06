8e727c34d90dd9c21c000be367df132015972b90
Jessica-Naegele <jessica.a.naegele@gmail.com>
2026-08-06 15:29:20 +0200

    13-0-7

--- deep_learning/dl_with_keras/9-tensorboard.py
+++ deep_learning/dl_with_keras/9-tensorboard.py
@@ -13,7 +13,7 @@ def log_to_tensorboard(log_dir, model, X, Y, epochs, verbose=1):
     log of training metrics to TensorBoard
     configure a callback that:
     - logs training metrics (loss, accuracy etc) after each epoch
-    - logs weight histograms and activation histogram 
+    - logs weight histograms and activation histogram
         (histogram_freq = 1) to help visualize weights evolve
     - Saves logs with unique timepstamp in format YYYYMMDD-HHMMSS
 
@@ -24,18 +24,18 @@ def log_to_tensorboard(log_dir, model, X, Y, epochs, verbose=1):
     - Y: labesl, shape(# of examples)
     - epochs: # epochs
     - verbose: verbosity model 0 - silent, 1 - progress bar
-    
+
     return: None
     """
 
     log_name = datetime.datetime.now().strftime("%Y%m%d-%H%M%S")
-    log_dir = log_dir + "/" + log_name
+    path = f"{log_dir}/{log_name}"
     cb = keras.callbacks.TensorBoard(
-        log_dir=log_dir,
-        histogram_freq=1
+        log_dir=path,
+        histogram_freq=1,
         update_freq='epoch'
     )
 
-    model.fit(X, Y, epochs=epochs, verbose=verbose, callbacks=cb)
+    model.fit(X, Y, epochs=epochs, verbose=verbose, callbacks=[cb])
 
     return None
