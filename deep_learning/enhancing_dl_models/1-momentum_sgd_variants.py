fe83d5810c4f3421e58a7cb0a4952367a9ebb736
Jessica-Naegele <jessica.a.naegele@gmail.com>
2026-08-10 16:50:19 +0200

    14-0-1

--- deep_learning/enhancing_dl_models/1-momentum_sgd_variants.py
+++ deep_learning/enhancing_dl_models/1-momentum_sgd_variants.py
@@ -28,6 +28,8 @@ def get_optimizer_SGD(name, lr, momentum=0.0, nesterov=False):
         nesterov = False
     elif name == "SGD+Momentum":
         nesterov = False
+    elif name == "SGD+Momentum+Nesterov":
+        nesterov = True
 
     optimizer = keras.optimizers.SGD(
         learning_rate=lr,
