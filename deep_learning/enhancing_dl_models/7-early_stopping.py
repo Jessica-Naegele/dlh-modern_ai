20ecd89ad63d11100225afcdeda26fdbff1f1bfb
Jessica-Naegele <jessica.a.naegele@gmail.com>
2026-08-13 10:24:02 +0200

    14-0-7

---
+++ deep_learning/enhancing_dl_models/7-early_stopping.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python3
+"""
+--- TASK 7 ---
+function with customizable early stopping callback
+"""
+
+from tensorflow import keras
+
+
+def get_early_stopping_callback(patience, monitor='val_loss', verbose=1):
+    """
+    customizable early stopping callback
+    - monitor a specific metric during training
+    - stop training if no improvement is seen after a defined number of epochs
+    - restore best model weights once training stop
+
+
+    args:
+    - patience: (int) # epochs to wait wihtout improv
+    - monitor: (str) metric to monitor (val_loss, val_accuracy)
+    - verbose: (int) verbosity mode to display messages
+
+    returns:
+    - configured early Stopping callback
+    """
+
+    cb = keras.callbacks.EarlyStopping(monitor=monitor, patience=patience, verbose=verbose)
+
+    return cb
