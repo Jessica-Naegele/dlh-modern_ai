aa46c0ef3ba96b93e25815612b55a6718d5cfd7b
Jessica-Naegele <jessica.a.naegele@gmail.com>
2026-08-16 14:48:05 +0200

    14-0-9

--- deep_learning/enhancing_dl_models/9-initiate_tuner.py
+++ deep_learning/enhancing_dl_models/9-initiate_tuner.py
@@ -1,9 +1,9 @@
 #!/usr/bin/env python3
 """
 --- TASK 9 Hyperparameter Tuning - Initiate the Tuner
-This function is initializing a Keras Tuner for hyperparameter tuning. Arguments
-provided to this functions are tuner_type, build_model, x and y values, seed,
-hyperband_iterations, max_trials)
+This function is initializing a Keras Tuner for hyperparameter
+tuning. Arguments provided to this functions are tuner_type,
+build_model, x and y values, seed, hyperband_iterations, max_trials)
 """
 
 import keras_tuner
@@ -18,49 +18,53 @@ def initiate_tuner(
     objective,
     overwrite=True
 ):
-  """
-  initiating hyperparameter tuning
 
-    args:
-    - tuner_type (str)
-      - Hyperband
-      - RandomSearch
-      - BayesianOptimization
-    - build_model (function) - returning a compiled model
-    - x_train (ndarray) Training features
-    - y_train (ndarray) Training labels
-    - seed (int) - random seed
-    - hyperband_iterations (int) - # of iterations
-    - max_trials (int) max trials for RandomSearch and BaysianOptimization
-    - objective (str) - Metric to optimize during tuning
-    - overwrite (bool) Default True (whether to overwrite previous tuning project)
+    """
+    initiating hyperparameter tuning
 
-    return
-    turner
-  """
+        args:
+        - tuner_type (str)
+        - Hyperband
+        - RandomSearch
+        - BayesianOptimization
+        - build_model (function) - returning a compiled model
+        - x_train (ndarray) Training features
+        - y_train (ndarray) Training labels
+        - seed (int) - random seed
+        - hyperband_iterations (int) - # of iterations
+        - max_trials (int) max trials for RandomSearch and
+            BaysianOptimization
+        - objective (str) - Metric to optimize during tuning
+        - overwrite (bool)
+            Default True (whether to overwrite previous tuning
+            project)
 
-  if tuner_type == "Hyperband":
-    tuner = keras_tuner.Hyperband(
-        hypermodel=build_model,
-        objective=objective,
-        hyperband_iterations=hyperband_iterations,
-        seed=seed
-      )
+        return
+        turner
+    """
 
-  elif tuner_type == 'RandomSearch':
-    tuner = keras_tuner.RandomSearch(
-        hypermodel=build_model,
-        objective=objective,
-        max_trials=max_trials,
-        seed=seed
+    if tuner_type == "Hyperband":
+        tuner = keras_tuner.Hyperband(
+            hypermodel=build_model,
+            objective=objective,
+            hyperband_iterations=hyperband_iterations,
+            seed=seed
         )
 
-  elif tuner_type == "BayesianOptimization":
-    tuner = keras_tuner.RandomSearch(
-          hypermodel=build_model,
-          objective=objective,
-          max_trials=max_trials,
-          seed=seed    
-      )
-      
-  return tuner
+    elif tuner_type == 'RandomSearch':
+        tuner = keras_tuner.RandomSearch(
+            hypermodel=build_model,
+            objective=objective,
+            max_trials=max_trials,
+            seed=seed
+            )
+
+    elif tuner_type == "BayesianOptimization":
+        tuner = keras_tuner.RandomSearch(
+            hypermodel=build_model,
+            objective=objective,
+            max_trials=max_trials,
+            seed=seed
+        )
+
+    return tuner
