d4e2527ef919c7f67ee59b162ed3a3b3c4f31022
Jessica-Naegele <jessica.a.naegele@gmail.com>
2026-08-24 11:37:16 +0200

    15-0-0

--- deep_learning/cv_architectures/0-create_cnn_model.py
+++ deep_learning/cv_architectures/0-create_cnn_model.py
@@ -7,54 +7,54 @@ Arguments: input_shape, filters, kernel_sizes, activations, pooling_type
 
 from tensorflow import keras
 
+
 def create_cnn_model(
     input_shape,
     filters,
     kernel_sizes,
     activations,
     pooling_type='max'
-    ):
-  """
-  creating a convolutional neural network model
+):
+    """
+    creating a convolutional neural network model
 
-  args:
-  - input_shape (tuple): shape of input data
-  - filters (list): # filters in each convolutional layer
-  - kernel_sizes (list): size of kernels for each convolutional layer
-  - activations (list): activation function for each convolutional layer
-  - pooling_type (str): typoe fo pooling ('max' (default), or 'avg')
+    args:
+    - input_shape (tuple): shape of input data
+    - filters (list): # filters in each convolutional layer
+    - kernel_sizes (list): size of kernels for each convolutional layer
+    - activations (list): activation function for each convolutional layer
+    - pooling_type (str): typoe fo pooling ('max' (default), or 'avg')
 
-  return:
-  - compiled CNN model
-  """
+    return:
+    - compiled CNN model
+    """
 
-  # set up sequential neural network with initial layer
-  model = keras.Sequential([
-       keras.layers.Input(shape=input_shape)
-  ])
-  
-  # hidden Convolutional layers
-  for i in range (0,len(filters)):
-    model.add(keras.layers.Conv2D(
-        filters=filters[i],
-        kernel_size=kernel_sizes[i],
-        activation=activations[i]
-    ))
-    if pooling_type == 'max':
-      model.add(keras.layers.MaxPooling2D((2,2)))
-    else:
-      model.add(keras.layers.AveragePooling2D(2,2))
-  
-  # flatten to make it dense
-  model.add(keras.layers.Flatten())
-  model.add(keras.layers.Dense(units=10, activation='softmax'))
+    # set up sequential neural network with initial layer
+    model = keras.Sequential([
+        keras.layers.Input(shape=input_shape)
+    ])
 
-  # compiling
-  model.compile(
-      optimizer='adam',
-      loss='sparse_categorical_crossentropy',
-      metrics=['accuracy']
-  )
+    # hidden Convolutional layers
+    for i in range(0, len(filters)):
+        model.add(keras.layers.Conv2D(
+            filters=filters[i],
+            kernel_size=kernel_sizes[i],
+            activation=activations[i]
+        ))
+        if pooling_type == 'max':
+            model.add(keras.layers.MaxPooling2D((2, 2)))
+        else:
+            model.add(keras.layers.AveragePooling2D(2, 2))
 
-  
-  return model
+    # flatten to make it dense
+    model.add(keras.layers.Flatten())
+    model.add(keras.layers.Dense(units=10, activation='softmax'))
+
+    # compiling
+    model.compile(
+        optimizer='adam',
+        loss='sparse_categorical_crossentropy',
+        metrics=['accuracy']
+    )
+
+    return model
